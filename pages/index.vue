f790cc479b10c42c0e17262b0b3888279fb73713
johao70 <perlazajohao@gmail.com>
2020-03-22 11:14:02 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-red-700 h-screen w-screen">
+    <logo />
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
